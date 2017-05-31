1d2e7fa21254f857031d0ed585a8f6da66b14773
jeffryporter <jporter7@gmail.com>
2017-05-31 16:03:52 -0400

    Initial commit

---
+++ src/main/java/com/alphaprojects/aaronandporter/Psdbalpha/PsdbalphaApplication.java
@@ -0,0 +1,12 @@
+package com.alphaprojects.aaronandporter.Psdbalpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsdbalphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsdbalphaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alphaprojects/aaronandporter/Psdbalpha/PsdbalphaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alphaprojects.aaronandporter.Psdbalpha;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PsdbalphaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
