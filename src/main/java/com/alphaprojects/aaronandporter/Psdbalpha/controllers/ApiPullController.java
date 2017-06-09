649d54d5b2825c6a09a72229f859f252319d25a8
jeffryporter <jporter7@gmail.com>
2017-06-09 06:50:50 -0400

    Add Pseudocode to api controller.

--- src/main/java/com/alphaprojects/aaronandporter/Psdbalpha/controllers/ApiPullController.java
+++ src/main/java/com/alphaprojects/aaronandporter/Psdbalpha/controllers/ApiPullController.java
@@ -9,7 +9,17 @@ public class ApiPullController
 {
     //grab federal data and put it in the database(google and congress.gov APIs)
 
+    //sort through federal officials/politicians
+
+    //sort through federal legislation
+
+
     // grab state data and put it in the database(google and openstates APIs)
+
+    //sort through state officials/politicians
+
+    //sort through state legislation
+
 
     //grab local data and pray to god it still works(No idea what to do here.)
 }
